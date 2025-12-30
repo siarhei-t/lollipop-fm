26451e695859af6a8d830db8152a7ca9e4b3e314
Siarhei Tatarchanka <zlojdigger@gmail.com>
2025-12-30 21:26:33 +0100

    - added vscode project, tasks and minimal build

--- firmware/stm32c0/cmake/gcc-arm-none-eabi.cmake
+++ firmware/stm32c0/cmake/gcc-arm-none-eabi.cmake
@@ -3,11 +3,20 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# Prevent CMake from testing the compiler
+set(CMAKE_C_COMPILER_FORCED TRUE)
+set(CMAKE_CXX_COMPILER_FORCED TRUE)
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_CXX_COMPILER_ID GNU)
+
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-# Toolchain prefix
-set(TOOLCHAIN_PREFIX arm-none-eabi-)
+# Absolute path to ARM GNU Toolchain
+set(TOOLCHAIN_DIR
+    /Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/bin
+)
+set(TOOLCHAIN_PREFIX
+    ${TOOLCHAIN_DIR}/arm-none-eabi-
+)
 
 # Compilers
 set(CMAKE_C_COMPILER   ${TOOLCHAIN_PREFIX}gcc)
@@ -19,32 +28,20 @@ set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
 set(CMAKE_SIZE    ${TOOLCHAIN_PREFIX}size)
 set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
 
-# Output suffix
 set(CMAKE_EXECUTABLE_SUFFIX ".elf")
 
-# MCU flags (STM32C0 = Cortex-M0)
-set(MCU_FLAGS "-mcpu=cortex-m0 -mthumb")
+# MCU flags (STM32C0 = Cortex-M0+)
+set(MCU_FLAGS "-mcpu=cortex-m0plus -mthumb")
 
-# Common compiler flags
-set(COMMON_FLAGS
-    "${MCU_FLAGS} \
-     -ffunction-sections \
-     -fdata-sections \
-     -Wall -Wextra -Wpedantic"
-)
+# Common flags
+set(COMMON_FLAGS "${MCU_FLAGS} -ffunction-sections -fdata-sections -Wall -Wextra -Wpedantic")
 
 set(CMAKE_C_FLAGS   "${COMMON_FLAGS}")
 set(CMAKE_ASM_FLAGS "${COMMON_FLAGS} -x assembler-with-cpp")
 set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -fno-rtti -fno-exceptions")
 
-# Build type flags
+# Build types
 set(CMAKE_C_FLAGS_DEBUG   "-O0 -g3")
 set(CMAKE_C_FLAGS_RELEASE "-Os -g0")
 
-# Linker flags
-set(CMAKE_EXE_LINKER_FLAGS
-    "${MCU_FLAGS} \
-     --specs=nano.specs \
-     -Wl,--gc-sections \
-     -Wl,--print-memory-usage"
-)
+set(CMAKE_EXE_LINKER_FLAGS "${MCU_FLAGS} --specs=nano.specs -Wl,--gc-sections -Wl,--print-memory-usage -lc -lm -lnosys")
